44c0c51b45e3a2c045d0569f3a5d62c67e1b8876
hurshpatel <hursh.patel2125@gmail.com>
2015-04-28 01:16:09 -0400

    Update search.php
    added the code for the 'executive' queries

--- search.php
+++ search.php
@@ -54,6 +54,107 @@ elseif ($selected_radio == "artist") {
     echo "producer";
 } elseif($selected_radio == "executive") {
 
+    $artist_name = $_POST['artistname'];  
+	$act_name = $_POST['actname'];                                                           
+    $album_name = $_POST['albumname'];                                                               
+    $song_name = $_POST['songname'];                                                                 
+    $year = $_POST['releaseyear']; 
+    $contract_years = $_POST['contractyears'];
+    $genre = $_POST['genre'];    
+    
+//query 1
+//albums released in a certain year
+	if(!empty($year)){
+		
+		$sql = "SELECT Act.ActName, Album.AlbumName, Album.Year
+				FROM Act, Album, Discography
+				WHERE Album.Year = \"$year\" 
+				AND Act.Act_DiscographyID = Album.Albums_DiscographyID";
+				
+		$result = mysql_query($sql);
+		
+		if($result == false){
+		die(mysql_error());
+		}
+		
+		while ($row = mysql_fetch_assoc($result)){
+		echo $row['ArtistName'];
+		echo $row['AlbumName'];
+		echo $row['Year'];
+		}
+	}
+	
+//query 2
+//artists who have X years or less on their contracts
+	else if(!empty($contract_years)){
+		
+		$sql = "SELECT Artist.ArtistName, Artist.ContractYears
+				FROM Artist
+				WHERE Artist.ContractYears <= \"$contract_years\"";	
+				
+		$result = mysql_query($sql);
+		
+		if($result == false){
+		die(mysql_error());
+		}
+		
+		while ($row = mysql_fetch_assoc($result)){
+		echo $row['ArtistName'];
+		echo $row['ContractYears'];
+		}
+	}
+
+//query 3
+//search acts in a certain genre
+
+	else if(!empty($genre)){
+	
+		$sql = "SELECT Act.ActName, Act.Genre
+				FROM Act
+				WHERE Act.genre = \"$genre\"";
+				
+		$result = mysql_query($sql);
+		
+		if($result == false){
+		die(mysql_error());
+		}
+		
+		while ($row = mysql_fetch_assoc($result)){
+		echo $row['actname'];
+		echo $row['genre'];
+		}
+	}
+
+//query for all the data allowed
+//all data allowed, so join all tables?
+
+	else if(all empty){
+	
+	$sql = "SELECT *
+			FROM Act, Albums, Artist, Discography, Employee, Executives, Producers, Songs
+			WHERE Employee.ExecutiveID = Executives.ExecutiveID
+			AND Employee.ProducerID = Producers.ProducerID
+			AND Employee.ArtistID = Artist.ArtistID
+			AND Artist.ActName = Act.ActName
+			AND Discography.DiscographyID = Act.DiscographyID
+			AND Discography.DiscographyID = Albums.DiscographyID
+			AND Discography.DiscographyID = Song.DiscographyID
+			AND Albums.AlbumName = Song.AlbumName";
+	
+	$result = mysql_query($sql);
+		
+		if($result == false){
+		die(mysql_error());
+		}
+		
+		while ($row = mysql_fetch_assoc($result)){
+		echo $row['actname'];
+		echo $row['genre'];
+		}
+	} 
+
+//query for data not allowed to user (but this user can access all data)    
+
     echo "executive";
 }
 
